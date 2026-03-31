efe4ac3e973a6685ceef950b512023c24de8bf50
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-03-31 07:24:51 -0300

    .

--- teste.js
+++
@@ -1,7 +0,0 @@
-async function teste() {
-    const resultado = await fetch("https://rest-server-one.vercel.app/teste")
-    const data = await resultado.json()
-    console.log(data.message)
-}
-
-teste()
