7e5145fab18c89ba8c820afbed9a9d33fabdcd32
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-03-31 11:47:43 -0300

    .

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port : process.env.DB_PORT
 })
 
 export default db
