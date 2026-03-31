af1da99050612d7b0ee0a906d347b45daab224e2
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-03-31 07:43:14 -0300

    .

--- routes/acessoRouter.js
+++ routes/acessoRouter.js
@@ -10,18 +10,19 @@ const router = express.Router()
     e envia o token de acesso se esse login
     existir no banco de dados
 */
-async function logar(req,res) {
+async function logar(req, res) {
     try {
-        const {email,senha} = req.body
-        const [resultado] = await db.execute("SELECT * FROM usuarios WHERE email = ? AND senha = ?", [email,senha])
+        const { email, senha } = req.body
+        const [resultado] = await db.execute("SELECT * FROM usuarios WHERE email = ? AND senha = ?", [email, senha])
         if (resultado.length > 0) {
             const acesso = jwt.sign({
-                email: resultado[0].email},
-            process.env.SECRET,
-            { expiresIn : "1h"})
-            return res.status(200).json({message: "Logado com sucesso",token: acesso})
+                email: resultado[0].email
+            },
+                process.env.SECRET,
+                { expiresIn: "1h" })
+            return res.status(200).json({ message: "Logado com sucesso", token: acesso })
         } else {
-            return res.status(401).json({message: "Usuario inexistente"})
+            return res.status(401).json({ message: "Usuario inexistente" })
         }
     } catch (erro) {
         console.error(erro)
@@ -35,19 +36,19 @@ async function logar(req,res) {
     o nome ou email está indisponivel com o codigo de status
     401 indicando falha
 */
-async function cadastrar(req,res) {
+async function cadastrar(req, res) {
     try {
-        const {email,senha,nome} = req.body
-        const [resultado] = await db.execute("INSERT INTO usuarios(nome,email,senha) VALUES(?,?,?)", [nome,email,senha])
+        const { email, senha, nome } = req.body
+        const [resultado] = await db.execute("INSERT INTO usuarios(nome,email,senha) VALUES(?,?,?)", [nome, email, senha])
         if (resultado.affectedRows > 0) {
-            return res.status(201).json({message: "Cadastrado com sucessso"})
+            return res.status(201).json({ message: "Cadastrado com sucessso" })
         } else {
-            return res.status(401).json({message: "Erro ao cadastrar"})
+            return res.status(401).json({ message: "Erro ao cadastrar" })
         }
     } catch (erro) {
         console.error(erro)
-        if (erro.code === "ER_DUP_ENTRY"){
-            res.status(400).json({message: "Email ou nome de usuario indisponivel"})
+        if (erro.code === "ER_DUP_ENTRY") {
+            res.status(400).json({ message: "Email ou nome de usuario indisponivel" })
         }
     }
 }
